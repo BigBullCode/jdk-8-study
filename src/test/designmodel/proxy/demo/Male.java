1a1300b8130892e33eafd199e454f5afb5a1a7a0
0027015428 <zdd_zhangdongdong@163.com>
2020-10-20 19:54:38 +0800

    代理模式

---
+++ src/test/designmodel/proxy/demo/Male.java
@@ -0,0 +1,14 @@
+package test.designmodel.proxy.demo;
+
+/**
+ * @Author: Zhangdongdong
+ * @Date: 2020/10/20 16:28
+ */
+public class Male implements Person {
+
+
+    @Override
+    public void findLove() {
+        System.out.println("有缘千里来相会");
+    }
+}
---
+++ src/test/designmodel/proxy/demo/Person.java
@@ -0,0 +1,11 @@
+package test.designmodel.proxy.demo;
+
+/**
+ * 人的行为：谈恋爱
+ * @Author: Zhangdongdong
+ * @Date: 2020/10/20 16:27
+ */
+public interface Person {
+
+    public void findLove();
+}
