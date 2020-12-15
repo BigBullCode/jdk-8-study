5fd073ba7e6ac141fdfd72be8704357f2890e486
0027015428 <zdd_zhangdongdong@163.com>
2020-12-15 19:39:33 +0800

    观察者模式

---
+++ src/test/designmodel/observer/example/demo/GPer.java
@@ -0,0 +1,29 @@
+package test.designmodel.observer.example.demo;
+import java.util.Observable;
+/**
+ * @Author: Zhangdongdong
+ * @Date: 2020/12/15 15:37
+ */
+public class GPer extends Observable {
+
+    private String name = "新野酒会";
+    private static GPer gPer = null;
+
+    private GPer() {}
+
+    public String getName() {
+        return name;
+    }
+    public static GPer getInstance() {
+        if (gPer == null) {
+            gPer = new GPer();
+        }
+        return gPer;
+    }
+
+    public void  publishQuestion(Question question) {
+        System.out.println(question.getUserName() + "在" + this.name + "上提交了一个问题。");
+        setChanged();
+        notifyObservers(question);
+    }
+}
---
+++ src/test/designmodel/observer/example/demo/Question.java
@@ -0,0 +1,23 @@
+package test.designmodel.observer.example.demo;
+/**
+ * @Author: Zhangdongdong
+ * @Date: 2020/12/15 15:39
+ */
+public class Question {
+
+    private String userName;
+    private String content;
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
